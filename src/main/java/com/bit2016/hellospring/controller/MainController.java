5a2ee444becceb0819f4930f160a801373af701c
bit <bit@DESKTOP-62FQSIC>
2018-11-27 17:24:47 +0900

    pom 수정

--- src/main/java/com/bit2016/hellospring/controller/MainController.java
+++ src/main/java/com/bit2016/hellospring/controller/MainController.java
@@ -10,21 +10,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-	
+
 	@ResponseBody
-	// 메서드 단독 request mapping 
-	@RequestMapping( "/main" )
+	// 메서드 단독 request mapping
+	@RequestMapping("/main")
 	public String main() {
 		return "main";
 	}
-	
+
 	// 비추천
 	// 기술이 침투되어 있다.
 	@ResponseBody
-	@RequestMapping( "/main2" )
-	public String main2( HttpServletRequest request, Writer out ) {
-		String name = request.getParameter( "name");
+	@RequestMapping("/main2")
+	public String main2(HttpServletRequest request, Writer out) {
+		String name = request.getParameter("name");
 		return "main:" + name;
 	}
-	
+
 }
